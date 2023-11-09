7e7e166b9a045113e2943581d3372db231af7519
SenkoTheFox <78004665+dudenotready@users.noreply.github.com>
2023-11-09 09:34:05 -0600

    Added Dark Mode

--- FrontEnd/chatgptapp.app/src/App.js
+++ FrontEnd/chatgptapp.app/src/App.js
@@ -1,15 +1,30 @@
 // src/App.js
-import React from 'react';
+import React, { useState } from 'react';
 import ChatContainer from './ChatContainer';
 import logo from './Logo.png'; // Correct relative path if logo.png is in the src directory
 import './App.css'; // Make sure you have an App.css file for your styles
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+    document.body.classList.toggle('dark-mode');
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" /> {/* Logo image */}
         <h1>Chat with ChatGPT</h1>
+        <label className="dark-mode-toggle">
+  <input 
+    type="checkbox" 
+    checked={isDarkMode} 
+    onChange={toggleDarkMode} 
+  />
+  Dark Mode
+</label>
       </header>
       <main>
         <ChatContainer />
@@ -17,5 +32,6 @@ const App = () => {
     </div>
   );
 }
+
 
 export default App;
