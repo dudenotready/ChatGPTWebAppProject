98bf133a2d0f8302200a7c1e0a8ea69528fd285c
SenkoTheFox <78004665+dudenotready@users.noreply.github.com>
2023-11-09 13:00:10 -0600

    Added the API call for the server.

--- Backend/server.js
+++ Backend/server.js
@@ -10,3 +10,22 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.post('/chat', async (req, res) => {
+  const { message } = req.body;
+
+  try {
+    const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
+      prompt: message,
+      max_tokens: 100
+    }, {
+      headers: {
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+      }
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
